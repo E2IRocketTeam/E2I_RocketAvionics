c01a44d68d24f5ddadf650e72e84c0fc5167371a
park-chu <parkchuy7077@kyonggi.ac.kr>
2025-02-06 14:40:46 +0900

    낙하산 서보 구현

--- Avionics_1.2/Avionics_1.2.ino
+++ Avionics_1.2/Avionics_1.2.ino
@@ -2,6 +2,8 @@
 #include "Sdcard.h"
 #include "BMP390.h"
 #include "BNO055.h"
+#include <Servo.h>
+
 
 const char* filename = "sensor_data.csv";
 BMP390 bmpSensor;
@@ -31,6 +33,8 @@ void setup() {
         Serial.println("Failed to create log file!");
         while (1);
     }
+    parachuteServo.attach(9); // 서보 모터 핀 (PWM 핀 사용)
+    parachuteServo.write(0);  // 초기 위치 (낙하산 닫힘)
 }
 
 void loop() {
@@ -40,8 +44,13 @@ void loop() {
     bmpSensor.readData(temperature, pressure, altitude);
     bnoSensor.readData(yaw, pitch, roll);
 
+    if(pitch >= 45 && pitch <= 50) {
+        Serial.println("🚀 낙하산 전개!");
+        parachuteServo.write(90); // 90도 회전 (서보 모터가 낙하산을 전개하도록 설정)
+    }
+
+
     if (logData(filename, yaw, pitch, roll, temperature, pressure, altitude)) {
-        Serial.print("Logged Data: ");
         Serial.print(yaw, 2); Serial.print(", ");
         Serial.print(pitch, 2); Serial.print(", ");
         Serial.print(roll, 2); Serial.print(", ");
